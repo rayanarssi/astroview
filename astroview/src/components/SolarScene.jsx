2e825a1cb0d3438f1cb25a7ab1ce01b8184263ca
rayanarssi <145460408+rayanarssi@users.noreply.github.com>
2025-10-15 11:12:17 +0200

    FEAT(scene): SolarScene (part 1/6:  shell + Sun only)

---
+++ astroview/src/components/SolarScene.jsx
@@ -0,0 +1,26 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment, Stars } from "@react-three/drei";
+import Planet from "./Planet.jsx";
+import data from "../data/planets.json";
+
+export default function SolarScene() {
+  const sun = data.find((p) => p.id === "sun");
+
+  return (
+    <Canvas camera={{ position: [0, 5, 12], fov: 45 }}>
+      {/* background + lights */}
+      <color attach="background" args={["#000011"]} />
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[5, 5, 5]} intensity={1.2} />
+      <Environment preset="sunset" />
+      <Stars radius={50} depth={80} count={5000} factor={4} saturation={0} />
+
+      {/* SUN */}
+      <group userData={{ pid: sun.id }}>
+        <Planet path={sun.model} scale={sun.scale} />
+      </group>
+
+      <OrbitControls enablePan={false} />
+    </Canvas>
+  );
+}
